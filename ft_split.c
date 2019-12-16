b14fa08c3bbf5302a0413f2cf3605f558286da56
Benjamin Duroule <beduroul@z3r2p3.le-101.fr>
2019-12-16 20:18:05 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -19,7 +19,7 @@ int ft_spa(char a)
     return (a == ' ' || a == '\t' || a == '\n');
 }
 
-/*int count_word(char *str)
+int count_word(char *str)
 {
     int i = 0;
     int size = 0;
@@ -29,30 +29,59 @@ int ft_spa(char a)
         while (str[i] && ft_spa(str[i]))
             i++;
         if (!ft_spa(str[i] && str[i])){
-            printf("%c}\n", str[i]); size++;
+            size++;
             while (str[i] && !ft_spa(str[i]))
                 i++;
         }
     }
     return size;
-}*/
+}
+
+char *mall_word(char *str)
+{
+    int i = 0;
+    char *word;
+
+    while (str[i] && ft_spa(str[i]))
+        i++;
+    word = (char *)malloc(sizeof(char) * i + 1);
+    i = 0;
+    while (str[i] && !ft_spa(str[i]))
+    {
+        word[i] = str[i];
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
+}
 
 
-/*char    **ft_split(char *str)
+char    **ft_split(char *str)
 {
     char **tab;
     int i = 0;
 
-    tab = (char **)malloc(sizeof(char *) * 1000000);
-    while (str[i])
+    tab = (char **)malloc(sizeof(char *) * count_word(str) + 1);
+    while (*str)
     {
-        while (ft_spa(str[i]))
-            i++;
+        while (*str && ft_spa(*str))
+            str++;
+        if (!ft_spa(*str))
+        {
+            tab[i++] = mall_word(str);
+            while (*str && !ft_spa(*str))
+              str++;
+        }
     }
-}*/
+    tab[i] = NULL;
+    return tab;
+}
 
 int main(int ac, char **av)
 {
-    printf("%d\n" ,count_word(av[1]));
+    char **tab = ft_split(av[1]);
+
+    for (int i = 0; tab[i]; i++)
+        printf("{%s}\n", tab[i]);
     return 0;
 }
